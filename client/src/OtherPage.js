28c130803117ad096df99a11bb675fbd0ca54fd6
SAHB <s75ahmad@uwaterloo.ca>
2024-01-24 14:10:44 -0500

    fix OtherPage component class constructor error

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class OtherPage {
+class OtherPage extends Component {
     render() {
         return (
             <div>
